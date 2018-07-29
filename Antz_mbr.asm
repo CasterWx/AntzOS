2e045482ce8bf0eb9941dd29716445fe7f926db5
CasterWx <1325200471@qq.com>
2018-07-29 09:25:10 +0800

    访问硬盘并载入内核

--- Antz_mbr.asm
+++ Antz_mbr.asm
@@ -27,33 +27,6 @@ SECTION MBR vstart=0x7c00
   mov byte [gs:0x06],'z'
   mov byte [gs:0x07],0xB1
 
-  mov byte [gs:0x08],' '
-  mov byte [gs:0x09],0xCC
-
-  mov byte [gs:0x0A],'U'
-  mov byte [gs:0x0B],0x2B
-
-  mov byte [gs:0x0C],'h'
-  mov byte [gs:0x0D],0x6D
-
-  mov byte [gs:0x0E],'l'
-  mov byte [gs:0x0F],0x7E
-
-  mov byte [gs:0x10],' '
-  mov byte [gs:0x11],0x49
-
-  mov byte [gs:0x12],'K'
-  mov byte [gs:0x13],0xE5
-
-  mov byte [gs:0x14],'o'
-  mov byte [gs:0x15],0x8A
-
-  mov byte [gs:0x16],'n'
-  mov byte [gs:0x17],0x96
-
-  mov byte [gs:0x18],'e'
-  mov byte [gs:0x19],0x68
-
   mov eax,LOADER_START_SECTOR
   mov bx,LOADER_BASE_ADDR
   mov cx,1
