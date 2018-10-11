81234940c385e366c8e652fbc2eb6abdcd1d1f65
CasterWx <1325200471@qq.com>
2018-10-11 19:09:27 +0800

    add keyAction

--- main/bootpack.c
+++ main/bootpack.c
@@ -1,6 +1,7 @@
 #include "todo.h"
 #include <stdio.h>
 #include <string.h>
+
 extern struct FIFO8 keyfifo, mousefifo;
 void enable_mouse(void);
 void init_keyboard(void);
@@ -10,88 +11,120 @@ int write_x = 55 ;
 int write_y = 57 ;
 
 char* replace_char(char s[40]){
-	char *chr = "&";
-	if(strcmp(s,"1E")==0){
-			chr = "A" ;
-	}else if(strcmp(s,"30")==0){
-			chr = "B" ;
-	}else if(strcmp(s,"2E")==0){
-			chr = "C" ;
-	}else if(strcmp(s,"20")==0){
-			chr = "D" ;
-	}else if(strcmp(s,"12")==0){
-			chr = "E" ;
-	}else if(strcmp(s,"21")==0){
-			chr = "F" ;
-	}else if(strcmp(s,"22")==0){
-			chr = "G" ;
-	}else if(strcmp(s,"23")==0){
-			chr = "H" ;
-	}else if(strcmp(s,"17")==0){
-		chr = "I" ;
-	}else if(strcmp(s,"24")==0){
-		chr = "J" ;
-	}else if(strcmp(s,"25")==0){
-		chr = "K" ;
-	}else if(strcmp(s,"26")==0){
-		chr = "L" ;
-	}else if(strcmp(s,"32")==0){
-		chr = "M" ;
-	}else if(strcmp(s,"31")==0){
-		chr = "N" ;
-	}else if(strcmp(s,"18")==0){
-		chr = "O" ;
-	}else if(strcmp(s,"19")==0){
-		chr = "P" ;
-	}else if(strcmp(s,"10")==0){
-		chr = "Q" ;
-	}else if(strcmp(s,"13")==0){
-		chr = "R" ;
-	}else if(strcmp(s,"1F")==0){
-		chr = "S" ;
-	}else if(strcmp(s,"14")==0){
-		chr = "T" ;
-	}else if(strcmp(s,"16")==0){
-		chr = "U" ;
-	}else if(strcmp(s,"2F")==0){
-		chr = "V" ;
-	}else if(strcmp(s,"11")==0){
-		chr = "W" ;
-	}else if(strcmp(s,"2D")==0){
-		chr = "X" ;
-	}else if(strcmp(s,"15")==0){
-		chr = "Y" ;
-	}else if(strcmp(s,"2C")==0){
-		chr = "Z" ;
+	char *chr = "" ;
+	sprintf(chr,"%s",s);
+	if((strcmp(s,"1E")==0)||(strcmp(s,"9E")==0)){
+			chr = "a" ;
+	}else if((strcmp(s,"30")==0)||(strcmp(s,"B0")==0)){
+			chr = "b" ;
+	}else if((strcmp(s,"2E")==0)||(strcmp(s,"AE")==0)){
+			chr = "c" ;
+	}else if((strcmp(s,"20")==0)||(strcmp(s,"A0")==0)){
+			chr = "d" ;
+	}else if((strcmp(s,"12")==0)||(strcmp(s,"92")==0)){
+			chr = "e" ;
+	}else if((strcmp(s,"21")==0)||(strcmp(s,"A1")==0)){
+			chr = "f" ;
+	}else if((strcmp(s,"22")==0)||(strcmp(s,"A2")==0)){
+			chr = "g" ;
+	}else if((strcmp(s,"23")==0)||(strcmp(s,"A3")==0)){
+			chr = "h" ;
+	}else if((strcmp(s,"17")==0)||(strcmp(s,"97")==0)){
+		chr = "i" ;
+	}else if((strcmp(s,"24")==0)||(strcmp(s,"A4")==0)){
+		chr = "j" ;
+	}else if((strcmp(s,"25")==0)||(strcmp(s,"A5")==0)){
+		chr = "k" ;
+	}else if((strcmp(s,"26")==0)||(strcmp(s,"A6")==0)){
+		chr = "l" ;
+	}else if((strcmp(s,"32")==0)||(strcmp(s,"B2")==0)){
+		chr = "m" ;
+	}else if((strcmp(s,"31")==0)||(strcmp(s,"B1")==0)){
+		chr = "n" ;
+	}else if((strcmp(s,"18")==0)||(strcmp(s,"98")==0)){
+		chr = "o" ;
+	}else if((strcmp(s,"19")==0)||(strcmp(s,"99")==0)){
+		chr = "p" ;
+	}else if((strcmp(s,"10")==0)||(strcmp(s,"90")==0)){
+		chr = "q" ;
+	}else if((strcmp(s,"13")==0)||(strcmp(s,"93")==0)){
+		chr = "r" ;
+	}else if((strcmp(s,"1F")==0)||(strcmp(s,"9F")==0)){
+		chr = "s" ;
+	}else if((strcmp(s,"14")==0)||(strcmp(s,"94")==0)){
+		chr = "t" ;
+	}else if((strcmp(s,"16")==0)||(strcmp(s,"96")==0)){
+		chr = "u" ;
+	}else if((strcmp(s,"2F")==0)||(strcmp(s,"AF")==0)){
+		chr = "v" ;
+	}else if((strcmp(s,"11")==0)||(strcmp(s,"91")==0)){
+		chr = "w" ;
+	}else if((strcmp(s,"2D")==0)||(strcmp(s,"AD")==0)){
+		chr = "x" ;
+	}else if((strcmp(s,"15")==0)||(strcmp(s,"95")==0)){
+		chr = "y" ;
+	}else if((strcmp(s,"2C")==0)||(strcmp(s,"AC")==0)){
+		chr = "z" ;
+	}else if((strcmp(s,"39")==0)||(strcmp(s,"B9")==0)){
+		chr = " " ;
 	}
 	return chr ;
 }
 
 int flag = 1 ;
 
+// 指令缓存，但是因为中断响应的时间问题，终端输入速度要非常慢
+char  command[40]  ;
+void add_command(char *s)  {
+		if(strcmp(s," ")==0){
+			sprintf(command,"%s%s",command,"");
+		}else if(strcmp(s,"$")){
+				//忽略这种错误输入
+		}else {
+			sprintf(command,"%s%s",command,s);
+		}
+}
+
+void action_command(struct BOOTINFO *binfo){
+		// action command
+		// ls
+		// data
+		if(strcmp(command,"data")==0){
+			// get new data;
+			write_y += 19 ;
+			putfonts8_asc(binfo->vram, binfo->scrnx, 4, write_y, COL8_FFFFFF, "AntzOS in 2018");
+		}
+		sprintf(command,"%s","");
+}
+
 void key(struct BOOTINFO *binfo,char s[40]){
 	if(strcmp(s,"1C")==0){
+			action_command(binfo);
 			write_x = 58 ;
 			write_y += 19 ;
 			putfonts8_asc(binfo->vram, binfo->scrnx, 4, write_y, COL8_FFFFFF, "AntzOS>");
-	}else if((strcmp(s,"3B")==0)||(strcmp(s,"BB")==0)){
+	}else if((strcmp(s,"3B")==0)||(strcmp(s,"BB")==0)){  //关于F1的响应中断
+			sprintf(command,"%s","");
 			flag = 0 ;
 			new_pe(binfo);
-	}else if(strcmp(s,"39")==0){
-			putfonts8_asc(binfo->vram, binfo->scrnx,  write_x,  write_y, COL8_FFFFFF, " ");
-			write_x += 8 ;
 	}else if(strcmp(s,"0E")==0){
 			// 回退
 			write_x -= 8 ;
 			boxfill8(binfo->vram, binfo->scrnx , COL8_000000,  write_x,     write_y,     write_x+19, write_y+19);
-			if(write_x<=58){ 
+			if(write_x<=58){
 				write_x = 146 ;
 				write_y -= 19 ;
 			}
 	}else {
 			//putfonts8_asc(binfo->vram, binfo->scrnx,  write_x,  write_y, COL8_FFFFFF, s);
 			putfonts8_asc(binfo->vram, binfo->scrnx,  write_x,  write_y, COL8_FFFFFF, replace_char(s));
+			add_command(replace_char(s));
 			write_x += 8 ;
+			// 添加响应区
+			//清除
+			boxfill8(binfo->vram, binfo->scrnx, COL8_008400 , 300	,240	,310	,250);
+			//打印字符 Only use debug
+			putfonts8_asc(binfo->vram, binfo->scrnx,  300,  240 ,COL8_000000, s) ;
 	}
 	if(write_x>148){
 		write_x = 58 ;
