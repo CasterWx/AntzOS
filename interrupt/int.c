02748eda6654655e43f8ecf59b94881f4a132293
CasterWx <1325200471@qq.com>
2018-10-13 00:17:11 +0800

    interrupt

--- interrupt/int.c
+++ interrupt/int.c
@@ -1,25 +1,22 @@
-/*初始化关系 */
-
 #include "todo.h"
 
 void init_pic(void)
 /* PIC初始化 */
 {
-	io_out8(PIC0_IMR,  0xff  ); /* 禁止所有中断 */
-	io_out8(PIC1_IMR,  0xff  ); /* 禁止所有中断 */
-
-	io_out8(PIC0_ICW1, 0x11  ); /* 边缘触发模式（edge trigger mode） */
-	io_out8(PIC0_ICW2, 0x20  ); /* IRQ0-7由INT20-27接收 */
-	io_out8(PIC0_ICW3, 1 << 2); /* PIC1由IRQ2相连 */
-	io_out8(PIC0_ICW4, 0x01  ); /* 无缓冲区模式 */
-
-	io_out8(PIC1_ICW1, 0x11  ); /* 边缘触发模式（edge trigger mode） */
-	io_out8(PIC1_ICW2, 0x28  ); /* IRQ8-15由INT28-2f接收 */
-	io_out8(PIC1_ICW3, 2     ); /* PIC1由IRQ2连接 */
-	io_out8(PIC1_ICW4, 0x01  ); /* 无缓冲区模式 */
-
-	io_out8(PIC0_IMR,  0xfb  ); /* 11111011 PIC1以外全部禁止 */
-	io_out8(PIC1_IMR,  0xff  ); /* 11111111 禁止所有中断 */
+	 /* 禁止中断 */
+	io_out8(PIC0_IMR,  0xff  );
+	io_out8(PIC1_IMR,  0xff  );  
+	io_out8(PIC0_ICW1, 0x11  );
+	io_out8(PIC0_ICW2, 0x20  );
+	io_out8(PIC0_ICW3, 1 << 2);
+	io_out8(PIC0_ICW4, 0x01  );
+	io_out8(PIC1_ICW1, 0x11  );
+	io_out8(PIC1_ICW2, 0x28  );
+	io_out8(PIC1_ICW3, 2     );
+	io_out8(PIC1_ICW4, 0x01  );
+	/* 禁止其他中断 */
+	io_out8(PIC0_IMR,  0xfb  );
+	io_out8(PIC1_IMR,  0xff  );
 
 	return;
 }
@@ -28,12 +25,12 @@ void init_pic(void)
 
 struct FIFO8 keyfifo;
 
+/* 键盘中断 */
 void inthandler21(int *esp)
-/* 来自PS/2键盘的中断 */
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	unsigned char data, s[4];
-	io_out8(PIC0_OCW2, 0x61);	/* 通知PIC IRQ-01 已经受理完毕 */
+	io_out8(PIC0_OCW2, 0x61);
 	data = io_in8(PORT_KEYDAT);
 	fifo8_put(&keyfifo, data);
 	return;
@@ -42,21 +39,19 @@ void inthandler21(int *esp)
 struct FIFO8 mousefifo;
 
 void inthandler2c(int *esp)
-/* 来自PS/2鼠标的中断 */
 {
 	unsigned char data;
-	io_out8(PIC1_OCW2, 0x64);	/* 通知PIC IRQ-12 已经受理完毕 */
-	io_out8(PIC0_OCW2, 0x62);	/* 通知PIC IRQ-02 已经受理完毕 */
+	io_out8(PIC1_OCW2, 0x64);
+	io_out8(PIC0_OCW2, 0x62);
 	data = io_in8(PORT_KEYDAT);
 	fifo8_put(&mousefifo, data);
 	return;
 }
 
+
+/* 只是接收，不做其他操作 */
 void inthandler27(int *esp)
-/* PIC0中断的不完整策略 */
-/* 这个中断在Athlon64X2上通过芯片组提供的便利，只需执行一次 */
-/* 这个中断只是接收，不执行任何操作 */ 
 {
-	io_out8(PIC0_OCW2, 0x67); /* 通知PIC的IRQ-07（参考7-1） */
+	io_out8(PIC0_OCW2, 0x67);
 	return;
 }
