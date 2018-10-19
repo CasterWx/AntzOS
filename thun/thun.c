a213c5b76e33956197205b433655643c079f3bf2
wber <1325200471@qq.com>
2018-10-19 22:35:17 +0800

    pri test

--- thun/thun.c
+++ thun/thun.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
-void to_printf(){
+void to_printf(int x,int y){
   struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
   // 打印测试
-  putfonts8_asc(binfo->vram, binfo->scrnx, 57, 57, COL8_FFFFFF, "new printf");
+  putfonts8_asc(binfo->vram, binfo->scrnx, x, y, COL8_FFFFFF, "new printf");
 }
