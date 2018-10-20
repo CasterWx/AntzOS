bcdd81bc6b74d62862caf749c1240ffe4a2d6c00
CasterWx <1325200471@qq.com>
2018-10-20 18:48:58 +0800

    other in

--- thun/thun.c
+++ thun/thun.c
@@ -5,5 +5,5 @@
 void to_printf(int x,int y){
   struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
   // 打印测试
-  putfonts8_asc(binfo->vram, binfo->scrnx, x, y, COL8_FFFFFF, "new printf");
+  print_string(binfo->vram, binfo->scrnx, x, y, COL8_FFFFFF, "new printf");
 }
