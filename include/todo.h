c650c55c6661b295e99132ac71117f974e97a19e
CasterWx <1325200471@qq.com>
2018-10-20 18:48:37 +0800

    change h

--- include/todo.h
+++ include/todo.h
@@ -1,5 +1,5 @@
 /* thun.c */
-void to_printf(void);
+void to_printf();
 
 /* asmhead.nas */
 struct BOOTINFO { /* 0x0ff0-0x0fff */
@@ -38,10 +38,11 @@ int fifo8_status(struct FIFO8 *fifo);
 /* graphic.c */
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+// 区域绘制
+void print_area(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 void init_screen8(char *vram, int x, int y);
-void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+void print_font(char *vram, int xsize, int x, int y, char c, char *font);
+void print_string(char *vram, int xsize, int x, int y, char c, unsigned char *s);
 void init_mouse_cursor8(char *mouse, char bc);
 void putblock8_8(char *vram, int vxsize, int pxsize,
 	int pysize, int px0, int py0, char *buf, int bxsize);
@@ -62,7 +63,7 @@ void putblock8_8(char *vram, int vxsize, int pxsize,
 #define COL8_008484		14
 #define COL8_848484		15
 
-/* dsctbl.c */
+/* graphic.c */
 struct SEGMENT_DESCRIPTOR {
 	short limit_low, base_low;
 	char base_mid, access_right;
