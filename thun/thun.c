a7ebc1223daf35ce19bbaafbf729c8291872b0b9
CasterWx <1325200471@qq.com>
2018-10-27 17:56:03 +0800

    delete

--- thun/thun.c
+++
@@ -1,9 +0,0 @@
-#include <todo.h>
-#include <stdio.h>
-#include <string.h>
-
-void to_printf(int x,int y){
-  struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-  // 打印测试
-  print_string(binfo->vram, binfo->scrnx, x, y, COL8_FFFFFF, "welcome come!");
-}
